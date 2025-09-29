ea9976df498e043b2746c4c906d7ad0428fd94b7
barani-2502 <barani@testpress.in>
2025-09-29 16:04:39 +0530

    Add api call to enroll a student in all available courses

--- courses/api/views.py
+++ courses/api/views.py
@@ -10,6 +10,8 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework import viewsets
 from .serializers import CourseSerializer
 from rest_framework.decorators import action
+from .permissions import IsEnrolled
+from .serializers import CourseWithContentsSerializer
 
 class SubjectListView(generics.ListAPIView):
     queryset = Subject.objects.all()
@@ -31,3 +33,11 @@ class CourseViewSet(viewsets.ReadOnlyModelViewSet):
         course = self.get_object()
         course.students.add(request.user)
         return Response({'enrolled': True})
+    
+    @action(detail=True,
+            methods=['post'],
+            serializer_class=CourseWithContentsSerializer,
+            authentication_classes=[BasicAuthentication],
+            permission_classes=[IsAuthenticated, IsEnrolled])
+    def contents(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
