4309489cbb050645c7b881f63dc8047197ad2733
barani-2502 <barani@testpress.in>
2025-09-29 15:21:13 +0530

    Add viewsets and authentication for students enroll in course

--- courses/api/views.py
+++ courses/api/views.py
@@ -5,6 +5,11 @@ from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from ..models import Course
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework import viewsets
+from .serializers import CourseSerializer
+from rest_framework.decorators import action
 
 class SubjectListView(generics.ListAPIView):
     queryset = Subject.objects.all()
@@ -13,9 +18,16 @@ class SubjectListView(generics.ListAPIView):
 class SubjectDetailView(generics.RetrieveAPIView):
     queryset = Subject.objects.all()
     serializer_class = SubjectSerializer
+    
+class CourseViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
 
-class CourseEnrollView(APIView):
-    def post(self, request, pk, format=None):
-        course = get_object_or_404(Course, pk=pk)
+    @action(detail=True,
+            methods=['post'],
+            authentication_classes=[BasicAuthentication],
+            permission_classes=[IsAuthenticated])
+    def enroll(self, request, *args, **kwargs):
+        course = self.get_object()
         course.students.add(request.user)
-        return Response({'enrolled':True})
+        return Response({'enrolled': True})
