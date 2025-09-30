e04c4a418cd961fabdd533632bfd4892090e1177
barani-2502 <barani@testpress.in>
2025-09-30 18:37:18 +0530

    Add requirements.txt

--- education/settings.py
+++ education/settings.py
@@ -139,7 +139,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 CACHES = {
     'default': {
         'BACKEND': 'django.core.cache.backends.memcached.PyMemcacheCache',
-        'LOCATION': '127.0.0.1:11211',
+        'LOCATION': '172.30.5.17:11211',
     }
 }
 
